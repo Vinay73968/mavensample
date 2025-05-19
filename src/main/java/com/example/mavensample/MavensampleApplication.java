9103dc13c89629bb917249e096de599f12fa922e
vinay77 <venkatagirivinay367@gmail.com>
2025-05-19 20:56:03 +0530

    initial commit

---
+++ src/main/java/com/example/mavensample/MavensampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.mavensample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavensampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavensampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mavensample/MavensampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mavensample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavensampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
